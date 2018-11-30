917e27624666cb418eda6b18b739c841c8edb290
Sungchan Yi <calofmijuck@snu.ac.kr>
2018-12-01 00:47:35 +0900

    Add editing and deleting

--- phone-book/src/components/PhoneInfo.js
+++ phone-book/src/components/PhoneInfo.js
@@ -9,6 +9,57 @@ class PhoneInfo extends Component {
         }
     }
 
+    state = {
+        // When edit button is clicked, editing value is set to true
+        // when this value is true, this will be shown in <input> form
+        editing: false,
+        // the value of <input> will vary. Set placeholders
+        name: '',
+        phone: '',
+    }
+
+
+    handleRemove = () => {
+        // When delete button clicked - call onRemove with id
+        const { info, onRemove } = this.props;
+        onRemove(info.id);
+    }
+
+    // Toggle Edit
+    handleToggleEdit = () => {
+        const { editing } = this.state;
+        this.setState({ editing: !editing });
+    }
+
+    // Called when input has onChange event
+    handleChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        // What to do when value of editing is changed
+        // When edit is triggered, the original value is shown in <input>
+        // When applying the modifications, the values will be passed to parent
+
+        const { info, onUpdate } = this.props;
+        if(!prevState.editing && this.state.editing) {
+            this.setState({
+                name: info.name,
+                phone: info.phone
+            });
+        }
+
+        if(prevState.editing && !this.state.editing) {
+            onUpdate(info.id, {
+                name: this.state.name,
+                phone: this.state.phone
+            });
+        }
+    }
+
     render() {
         const style = {
             border: '1px solid black',
@@ -16,14 +67,44 @@ class PhoneInfo extends Component {
             margin: '8px'
         };
 
+        const { editing } = this.state;
+
+        if(editing) {
+            return (
+                <div style={style}>
+                    <div>
+                        <input
+                            value={this.state.name}
+                            name="name"
+                            placeholder="Name"
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <div>
+                        <input
+                            value={this.state.phone}
+                            name="phone"
+                            placehlder="Phone Number"
+                            onChange={this.handleChange}
+                        />
+                    </div>
+                    <button onClick={this.handleToggleEdit}>Apply</button>
+                    <button onClick={this.handleRemove}>Delete</button>
+                </div>
+            );
+        }
+
         const {
-            name, phone, id
+            name, phone
         } = this.props.info;
 
         return (
             <div style={style}>
                 <div><b>{name}</b></div>
                 <div>{phone}</div>
+                <button onClick={this.handleToggleEdit}>Edit</button>
+
+                <button onClick={this.handleRemove}>Delete</button>
             </div>
         );
     }
